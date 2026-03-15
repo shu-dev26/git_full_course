f25b5abf88a66cd21ad6c01d81858d9b1ed2a2c4
TIANWEI.ZHOU <b649024921@gmail.com>
2026-03-15 22:14:53 +0900

    fix

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ function validateInput(v) { return !!v; }
 function getUser() {}
 function getUser(id) { return id; }
 function updateUser(id, data) {}
+function updateUser(id, data) { return data; }
